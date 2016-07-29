8cc498496ac25d3278099c4efa96387a7f027072
Marius Oprea <marius.oprea95@yahoo.ro>
2016-07-29 16:55:36 +0300

    Working on profile page

--- Websites/Teamslancer/assets/js/script.js
+++ Websites/Teamslancer/assets/js/script.js
@@ -19,3 +19,47 @@ jQuery(document).ready(function($){
 		);
 	});
 });
+
+$(".top").tooltip({
+    placement: "top"
+});
+
+$('#show-more').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+});
+
+$('#contacts-header').on('click', function(e) {
+    e.preventDefault();
+	var $contacts = $('#contacts');
+	if($contacts.hasClass('minimised')) {
+		$contacts.removeClass('minimised');
+		$('#contacts-settings').removeClass('display-none');
+	} else {
+		$contacts.addClass('minimised');
+		$('#contacts-settings').addClass('display-none');
+	}
+});
+
+
+var typingTimer;               
+var doneTypingInterval = 500;  
+var $input = $('#contacts-search-input');
+
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+$input.on('keydown', function () {
+	$('#contact-search-loader').removeClass('display-none');
+	$('#contacts-content-inner').addClass('display-none');	
+	clearTimeout(typingTimer);
+});
+
+function doneTyping () {
+	$('#contact-search-loader').addClass('display-none');
+	$('#contacts-content-inner').removeClass('display-none');
+}
